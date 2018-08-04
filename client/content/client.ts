f91344f28853de2f9f0e157343208df305f16766
Walter Rumsby <walter.rumsby@myfoodbag.co.nz>
2018-08-04 16:01:03 +1200

    I doubt this will work

--- client/content/client.ts
+++ client/content/client.ts
@@ -1,7 +1,7 @@
 import {createClient} from 'contentful';
 
-const space = process.env.CONTENTFUL_SPACE_ID || '';
-const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN || '';
+const space = process.env.CONTENTFUL_SPACE_ID;
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
 
 const contentfulClient = createClient({
   space,

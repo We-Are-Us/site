6f0edde19727f668410b7a9e3351106aeb0e3b2e
Walter Rumsby <walter.rumsby@myfoodbag.co.nz>
2018-08-04 13:29:26 +1200

    wrap in exception handler

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+try {
 require('dotenv-safe').load({
     allowEmptyValues: true
   });
+} catch (e) {
+  console.log('error', e);XX
+}
 
   require('./server/monitoring/index');
   require('./server/start');

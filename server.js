bd86a4e268cd49417141f633b06a856c6356be7e
Walter Rumsby <walter.rumsby@myfoodbag.co.nz>
2018-08-05 10:37:54 +1200

    Add app insights

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 try {
-require('dotenv-safe').load({
+  require('dotenv-safe').load({
     allowEmptyValues: true
   });
 } catch (e) {
   console.warn('Exception thrown when loading dotenv-safe');
 }
 
-  require('./server/monitoring/index');
-  require('./server/start');
+require('./server/monitoring/index');
+require('./server/start');
--- server/monitoring/index.ts
+++ server/monitoring/index.ts
@@ -1,3 +1,10 @@
 import logger from '../logging/logger';
+import {setup} from 'applicationinsights';
 
-logger.log('info', 'No monitoring.');
+if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+  setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start();
+
+  logger.log('debug', 'Applicationm Insights started.');
+} else {
+  logger.log('info', 'No monitoring.');
+}

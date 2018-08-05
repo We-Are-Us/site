15fa168d2849246f1a5deb7524dcc81a4da1a8ef
Walter Rumsby <walter.rumsby@myfoodbag.co.nz>
2018-08-05 21:33:06 +1200

    Update layout

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,39 +1,46 @@
 import * as React from 'react';
 
 const Navbar: React.SFC<{}> = () => (
-  <nav className="navbar navbar-expand-md navbar-light bg-dark text-white">
-    <a className="navbar-brand text-white" href="/">
-      <img src="/public/assets/Logo_White.svg" itemProp="logo" width="200" />
-    </a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
+  <div className="position-absolute w-100">
+    <nav className="navbar navbar-expand-md navbar-light text-white">
+      <a className="navbar-brand text-white" href="/">
+        <img src="/public/assets/Logo_White.svg" itemProp="logo" width="200" />
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ml-auto pt-1">
-        <li className="nav-item active pr-2">
-          <a className="nav-link text-white" href="#">Find a Practicioner <span className="sr-only">(current)</span></a>
-        </li>
-        <li className="nav-item px-2">
-          <a className="nav-link text-white" href="#">Register as a Practicioner</a>
-        </li>
-        <li className="nav-item px-2">
-          <a className="nav-link text-white" href="#">Sign up</a>
-        </li>
-        <li className="nav-item pl-2">
-          <a className="btn btn-outline-light" href="#">Login</a>
-        </li>
-      </ul>
-    </div>
-  </nav >
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto pt-1">
+          <li className="nav-item active pr-2">
+            <a
+              className="nav-link text-white"
+              href="#"
+            >
+              Find a Practicioner <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item px-2">
+            <a className="nav-link text-white" href="#">Register as a Practicioner</a>
+          </li>
+          <li className="nav-item px-2">
+            <a className="nav-link text-white" href="#">Sign up</a>
+          </li>
+          <li className="nav-item pl-2">
+            <a className="btn btn-outline-light" href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 );
 
 export default Navbar;
